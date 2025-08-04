2c6061dda7516ccc843482daf9b7ef111b74c6ad
sjwesq <sophie@sjwesq.dev>
2025-08-04 16:42:09 -0400

    Code reformatting

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 RED=$'\033[31m'
 YELLOW=$'\033[33m'
@@ -10,14 +10,14 @@ RESET=$'\033[0m'
 ################################################################################
 clear_line() {
   cols="$(tput cols)"
-  printf "%*s\r" "$((cols-1))" ""
+  printf "%*s\r" "$((cols - 1))" ""
 }
 
-wait_for_input () {
+wait_for_input() {
   read -n1 -s $1
 }
 
-timer_display () {
+timer_display() {
   local seconds=$1
   local label="$2"
 
@@ -25,13 +25,13 @@ timer_display () {
   local pause
   local skip
 
-  while [ "$seconds" -gt 0 ]; do
+  while [[ $seconds > 0 ]]; do
     clear_line
     printf -v seconds_formatted "%02d:%02d" $((seconds / 60)) $((seconds % 60))
     printf '%s %s\r' "$label" "$seconds_formatted"
 
     IFS= read -t1 -n1 pause # Doubles as a timer by using `-t`
-    if [[ "$pause" != "" ]]; then
+    if [[ $pause != "" ]]; then
       clear_line
       if [[ "$minimal_mode" ]]; then
         printf "%s %s ${YELLOW}[P]${RESET}\r" "$label" "$seconds_formatted"
@@ -41,20 +41,20 @@ timer_display () {
           "$label" "$seconds_formatted"
       fi
       wait_for_input skip
-      if [[ "$skip" = 's' ]]; then
+      if [[ $skip == 's' ]]; then
         clear_line
         break
       fi
       # Prevents the timer from rapidly decrementing with many inputs
-      seconds=$(($seconds + 1))
+      seconds=$((seconds + 1))
     fi
-    seconds=$(($seconds - 1))
+    seconds=$((seconds - 1))
   done
   clear_line
 }
 
 number_check() {
-  if ! [[ "$1" =~ ^[0-9]+$ ]]; then
+  if ! [[ $1 =~ ^[0-9]+$ ]]; then
     printf "${RED}ERROR${RESET}: '$1' is not a valid number.\n"
     exit 1
   fi
@@ -62,7 +62,7 @@ number_check() {
 
 require_audio() {
   soxi "$1" &>/dev/null
-  if [[ "$?" != 0 ]]; then
+  if [[ $? != 0 ]]; then
     printf "${RED}ERROR${RESET}: '$1' is not a supported audio file.\n"
     printf "See \`play -h\` for a list of supported formats.\n"
     exit 1
@@ -73,9 +73,9 @@ require_audio() {
 # Script Execution
 ################################################################################
 
-length_seconds_pomo=$((25*60))
-length_seconds_shortbreak=$((5*60))
-length_seconds_longbreak=$((20*60))
+length_seconds_pomo=$((25 * 60))
+length_seconds_shortbreak=$((5 * 60))
+length_seconds_longbreak=$((20 * 60))
 
 interval_longbreak=4
 cycle_current=1
@@ -102,16 +102,16 @@ while getopts 'hmwp:b:l:i:c:n:t:' OPTION; do
       printf "  -w\tEnables black-and-(w)hite output mode (disables color)\n\n"
 
       printf "  -p\tLength of (p)omodoros in minutes (default %d)\n" \
-        $((length_seconds_pomo/60))
+        $((length_seconds_pomo / 60))
       printf "  -b\tLength of (b)reaks in minutes (default %d)\n" \
-        $((length_seconds_shortbreak/60))
+        $((length_seconds_shortbreak / 60))
       printf "  -l\tLength of (l)ong breaks in minutes (default %d)\n\n" \
-        $((length_seconds_longbreak/60))
+        $((length_seconds_longbreak / 60))
 
       printf "  -c\tCurrent pomodoro (c)ycle (default %d)\n" \
         $cycle_current
       printf \
-        "  -i\t(i)nterval for how often long breaks are given (default %d)\n\n"\
+        "  -i\t(i)nterval for how often long breaks are given (default %d)\n\n" \
         $interval_longbreak
       printf "  -n\tLocation of pomodoro-end (n)otification sound file\n"
       printf "  -t\tLocation of break (t)ime-up sound file\n"
@@ -163,14 +163,13 @@ while getopts 'hmwp:b:l:i:c:n:t:' OPTION; do
   esac
 done
 
-command -v play &> /dev/null || {
+command -v play &>/dev/null || {
   printf "${YELLOW}WARNING${RESET}:"
   printf "'play' command not found. Install SoX for audio support.\n"
 }
 
-while :
-do
-  if [[ "$minimal_mode" = true ]]; then
+while true; do
+  if [[ $minimal_mode == true ]]; then
     label_current="${RED}PMO${RESET}#"
   else
     printf 'Press any key to start '
@@ -179,14 +178,14 @@ do
   printf "%s%d...\r" "$label_current" $cycle_current
   wait_for_input
   timer_display $length_seconds_pomo "${label_current}${cycle_current}"
-  play "$sound_notification" &> /dev/null &
+  play "$sound_notification" &>/dev/null &
 
-  if [[ $((cycle_current % interval_longbreak)) = 0 ]]; then
+  if [[ $((cycle_current % interval_longbreak)) == 0 ]]; then
     breaklength=$length_seconds_longbreak
   else
     breaklength=$length_seconds_shortbreak
   fi
-  if [[ "$minimal_mode" = true ]]; then
+  if [[ $minimal_mode == true ]]; then
     label_current="${CYAN}BRK${RESET}#"
   else
     printf 'Press any key to start '
@@ -195,7 +194,7 @@ do
   printf '%s%d...\r' "$label_current" $cycle_current
   wait_for_input
   timer_display $breaklength "${label_current}${cycle_current}"
-  play "$sound_timeup" &> /dev/null &
+  play "$sound_timeup" &>/dev/null &
 
-  cycle_current=$((cycle_current+1))
+  cycle_current=$((cycle_current + 1))
 done
